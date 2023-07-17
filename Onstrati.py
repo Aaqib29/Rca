b24f914f1f687bc71e8d95fa70ddd4c7daeb2a2c
Aaqib Shaikh <95633437+Aaqib29@users.noreply.github.com>
2023-07-18 00:44:37 +0530

    Create Onstrati.py

---
+++ Onstrati.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import re
+
+# Load the CSV file into a DataFrame
+df = pd.read_csv('data.csv')
+
+# Function to extract data based on RCA and Mitigation keywords
+def extract_rca_data(row):
+    regex_pattern = r'RCA:(.*?)(?=Mitigation|$)'  # Regular expression to extract data between RCA and Mitigation
+    match = re.search(regex_pattern, row)
+    if match:
+        return match.group(1).strip()
+    else:
+        return None
+
+# Function to extract stratification information
+def extract_stratification(row):
+    stratification_match = re.search(r'This offense was closed with reason: (.*?)\.', row, re.IGNORECASE)
+    if stratification_match:
+        stratification = stratification_match.group(1).strip()
+        return stratification
+    else:
+        return None
+
+# Apply the function to the DataFrame to create a new column 'rca_data'
+df['rca_data'] = df['allNotes'].apply(extract_rca_data)
+
+# Apply the function to extract stratification information and create a new column 'Stratification'
+df['Stratification'] = df['allNotes'].apply(extract_stratification)
+
+# Save the updated DataFrame back to a new CSV file
+df.to_csv('output_data.csv', index=False)
+
+# Print the resulting DataFrame
+print(df)
