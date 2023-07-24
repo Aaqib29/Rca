752cbce20f327caffea3e1ea7ca2da064ac0989e
Aaqib Shaikh <95633437+Aaqib29@users.noreply.github.com>
2023-07-25 02:56:57 +0530

    Update Hash.py

--- Hash.py
+++ Hash.py
@@ -1,29 +1,35 @@
 import pandas as pd
-import hashlib
+import requests
 
-def generate_sha256_hash(input_string):
-    sha256_hash = hashlib.sha256()
-    sha256_hash.update(input_string.encode('utf-8'))
-    return sha256_hash.hexdigest()
+def get_sha256_from_virustotal(api_key, hash_value):
+    url = f'https://www.virustotal.com/api/v3/files/{hash_value}'
+    headers = {'x-apikey': api_key}
+    response = requests.get(url, headers=headers)
+    
+    if response.status_code == 200:
+        data = response.json()
+        return data['data']['attributes']['sha256']
+    else:
+        return 'NA'
 
-def convert_hash_to_sha256(input_file, hash_column, output_file):
+def add_sha256_from_virustotal(input_file, output_file, api_key):
     # Read the CSV file into a pandas DataFrame
     df = pd.read_csv(input_file)
 
-    # Generate SHA-256 hashes for each value in the specified hash_column
-    df['sha256'] = df[hash_column].apply(generate_sha256_hash)
+    # Create a new column to store the SHA256 hashes from VirusTotal
+    df['sha256_virustotal'] = df['Hash'].apply(lambda x: get_sha256_from_virustotal(api_key, x))
 
-    # Save the DataFrame with the new "sha256" column to a new CSV file
+    # Save the DataFrame with the new column to a new CSV file
     df.to_csv(output_file, index=False)
 
 if __name__ == "__main__":
     # Replace 'input_file.csv' with the path to your input CSV file
     input_csv = "input_file.csv"
 
-    # Replace 'hash_column' with the name of the column containing the hash values
-    hash_column_to_convert = "hash_column"
-
     # Replace 'output_file.csv' with the desired name for the output CSV file
     output_csv = "output_file.csv"
 
-    convert_hash_to_sha256(input_csv, hash_column_to_convert, output_csv)
+    # Replace 'YOUR_VIRUSTOTAL_API_KEY' with your actual VirusTotal API key
+    virustotal_api_key = "YOUR_VIRUSTOTAL_API_KEY"
+
+    add_sha256_from_virustotal(input_csv, output_csv, virustotal_api_key)
