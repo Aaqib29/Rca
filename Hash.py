4896d2c1f4cffaef33f1fd8e85412ca1dc6436fe
Aaqib Shaikh <95633437+Aaqib29@users.noreply.github.com>
2023-07-25 02:49:58 +0530

    Update Hash.py

--- Hash.py
+++ Hash.py
@@ -6,24 +6,24 @@ def generate_sha256_hash(input_string):
     sha256_hash.update(input_string.encode('utf-8'))
     return sha256_hash.hexdigest()
 
-def add_sha256_hash_column(input_file, data_column, output_file):
+def convert_hash_to_sha256(input_file, hash_column, output_file):
     # Read the CSV file into a pandas DataFrame
     df = pd.read_csv(input_file)
 
-    # Generate SHA-256 hashes for the values in the specified data_column
-    df['sha256_hash'] = df[data_column].apply(generate_sha256_hash)
+    # Generate SHA-256 hashes for each value in the specified hash_column
+    df['sha256'] = df[hash_column].apply(generate_sha256_hash)
 
-    # Save the DataFrame with the new column containing the SHA-256 hashes to a new CSV file
+    # Save the DataFrame with the new "sha256" column to a new CSV file
     df.to_csv(output_file, index=False)
 
 if __name__ == "__main__":
     # Replace 'input_file.csv' with the path to your input CSV file
     input_csv = "input_file.csv"
 
-    # Replace 'data_column' with the name of the column whose values will be used to generate the SHA-256 hashes
-    data_column_to_hash = "data_column"
+    # Replace 'hash_column' with the name of the column containing the hash values
+    hash_column_to_convert = "hash_column"
 
     # Replace 'output_file.csv' with the desired name for the output CSV file
     output_csv = "output_file.csv"
 
-    add_sha256_hash_column(input_csv, data_column_to_hash, output_csv)
+    convert_hash_to_sha256(input_csv, hash_column_to_convert, output_csv)
