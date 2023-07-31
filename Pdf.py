aed7d1a2ed96fd3b3b447610d7a4e10fd077a115
Aaqib Shaikh <95633437+Aaqib29@users.noreply.github.com>
2023-08-01 04:32:13 +0530

    Update Pdf.py

--- Pdf.py
+++ Pdf.py
@@ -1,23 +1,24 @@
 import re
 import pandas as pd
-from PyPDF2 import PdfReader
+import fitz  # PyMuPDF
 
 def extract_hashes_from_pdf(pdf_file):
-    # Open the PDF file in read-binary mode
-    with open(pdf_file, 'rb') as file:
-        pdf_reader = PdfReader(file)
-        num_pages = len(pdf_reader.pages)
+    # Open the PDF file
+    pdf_document = fitz.open(pdf_file)
 
-        # Regular expression pattern for common hash types (e.g., MD5, SHA-256)
-        hash_pattern = r'[0-9a-fA-F]{32}|[0-9a-fA-F]{40}|[0-9a-fA-F]{64}'
+    # Regular expression pattern for common hash types (e.g., MD5, SHA-256)
+    hash_pattern = r'\b(?:[0-9a-fA-F]{32}|[0-9a-fA-F]{40}|[0-9a-fA-F]{64})\b'
 
-        hashes = []
-        # Loop through each page and find hashes using regex
-        for page_num in range(num_pages):
-            page = pdf_reader.pages[page_num]
-            page_text = page.extract_text()
-            found_hashes = re.findall(hash_pattern, page_text)
-            hashes.extend(found_hashes)
+    hashes = []
+    # Loop through each page and find hashes using regex
+    for page_num in range(pdf_document.page_count):
+        page = pdf_document.load_page(page_num)
+        page_text = page.get_text()
+        found_hashes = re.findall(hash_pattern, page_text)
+        hashes.extend(found_hashes)
+
+    # Close the PDF document
+    pdf_document.close()
 
     return hashes
 
